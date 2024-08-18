5e4ab95516d865cdbd5bfe3e75236db70fc0a9fc
ayush3160 <kshitij3160@gmail.com>
2024-08-18 12:03:16 +0530

    fix: fixed navabar for build issues

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -14,23 +14,23 @@ export default function Navbar() {
     setHydrated(true);
   }, []);
 
-  return (
-    hydrated && (
-      <nav className='bg-black shadow-md'>
-        <div className='container mx-auto px-4 py-4 flex justify-between items-center'>
-          <Link href='/' className='text-2xl font-bold text-white'>
-            Flipkart
-          </Link>
-          <Link href='/cart' className='text-white px-4 py-2 hidden md:block'>
-            <div className='relative'>
-              <span className='absolute top-0 right-0 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center transform translate-x-1/2 -translate-y-1/2'>
-                {itemsCount}
-              </span>
-              <BsCart4 className='text-2xl' />
-            </div>
-          </Link>
-        </div>
-      </nav>
-    )
+  return hydrated ? (
+    <nav className='bg-black shadow-md'>
+      <div className='container mx-auto px-4 py-4 flex justify-between items-center'>
+        <Link href='/' className='text-2xl font-bold text-white'>
+          Flipkart
+        </Link>
+        <Link href='/cart' className='text-white px-4 py-2 hidden md:block'>
+          <div className='relative'>
+            <span className='absolute top-0 right-0 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center transform translate-x-1/2 -translate-y-1/2'>
+              {itemsCount}
+            </span>
+            <BsCart4 className='text-2xl' />
+          </div>
+        </Link>
+      </div>
+    </nav>
+  ) : (
+    <></>
   );
 }
